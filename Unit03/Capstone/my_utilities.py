3b28d6d641f3abb224a0a8d8e771e882bac0b756
cmkeith9749 <cmkeith9749@gmail.com>
2019-09-18 10:19:27 -0400

    20190918 All Unit Four

--- Unit03/Capstone/my_utilities.py
+++ Unit03/Capstone/my_utilities.py
@@ -1,29 +1,30 @@
 
-	   
 def label_uncap_split (string):
-	dct = {'mssub':'sub', 'mszoning':'znng', 'mssub':'sub', '1st':'frst', '2nd':'scnd',
-	'exterior2nd':'extr_scnd', 'exter':'extr','exterior1st':'extr_frst' }
-	S = []   
-	for (i,c) in enumerate(string): 
-		if not i:
-			sub = c.lower(); c = ''; prv_upr = True
-		else:
-			if c.isupper():
-					if prv_upr:
-						prv_upr = True
-					else:
-						S.append(sub); sub = c.lower() ; c = ''; prv_upr = True
-			else:
-				prv_upr = False                
-			sub += c.lower()
-		if (i + 1 == len(string)):  
-			S.append(sub)
-		for (i, s) in enumerate(S):
-			if s in dct:
-				S[i] = dct[s]     
-	return '_'.join(S)
-
+    dct = {'mssub':'sub', 'mszoning':'znng', 'mssub':'sub', 'frontage':'frtg','exposure':'xpsr',
+           'exterior':'xtrr', 'exter':'xtrr', 'garage':'grg', 'year':'yr','condition':'cond',
+           'heating':'htng', 'qc':'qual'}
+    S=[]
+    for (i,c) in enumerate(string): 
+        # first char
+        if not i:
+            prv = True; bgn = i
+        # last char
+        elif i+1 == len(string):
+            if (c.isupper() or c.isdigit()) and not prv:
+                S.append(string[bgn:i].lower()); S.append(c.lower())
+            else:   
+                S.append(string[bgn:].lower())
+        # all the others        
+        else:
+            if c.isupper() or c.isdigit():
+                if not prv:
+                    S.append(string[bgn:i].lower()); bgn = i; prv = True
+            else:
+                prv = False
+    S = [dct[s] if s in dct else s for s in S]            
+    return '_'.join(S)
 # transform using dictionary 
+
 def fillna_transform_dct(srs, dict): 
     srs = srs.fillna('NA')
     return srs.apply(lambda x : dict[x])
